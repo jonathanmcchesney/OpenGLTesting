2c8903e38ffb2b6d6b530343daa59f775dc608ae
jonathanmcchesney <jmcchesney12@gmail.com>
2016-10-02 23:01:41 +0100

    Added Projection and Camera Transformations

--- app/src/main/java/com/example/jonathan/opengltest/MGLRenderer.java
+++ app/src/main/java/com/example/jonathan/opengltest/MGLRenderer.java
@@ -2,6 +2,8 @@ package com.example.jonathan.opengltest;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -11,6 +13,10 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class MGLRenderer implements GLSurfaceView.Renderer {
     private Triangle mTriangle;
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
         GLES20.glClearColor(0.1f, 0.0f, 0.0f, 1.0f); // Red - Green - Blue - Transpaency
@@ -20,11 +26,26 @@ public class MGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
+        //mTriangle.draw(mMVPMatrix);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw shape
+        mTriangle.draw(mMVPMatrix);
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
